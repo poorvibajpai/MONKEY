4799d608d8ae5fbca421ce3f9ef752af40795c17
Animesh Shukla <animeshshuklafoundation@gmail.com>
2024-04-04 23:23:32 +0530

    new update

--- src/components/Video.tsx
+++
@@ -1,8 +0,0 @@
-export default function Example() {
-    return (
-      <video className="h-full w-full rounded-lg" controls autoPlay muted>
-        <source src="https://docs.material-tailwind.com/demo.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    );
-  }
