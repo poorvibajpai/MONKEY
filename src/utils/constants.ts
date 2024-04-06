7b862c6b98b14681ed5e6a231556d8e944d43bc7
arpit <arpitsrivastava54@gmail.com>
2024-04-07 03:40:19 +0530

    uncomment prod base url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 // For local testing
-export const BASE_URL = "http://localhost:8000/api/v1";
+// export const BASE_URL = "http://localhost:8000/api/v1";
 
 // For production (on render)
 // export const BASE_URL = "https://ecommerce-backend-9fpg.onrender.com/api/v1";
 // For production (on cyclic)
-// export const BASE_URL = "https://nutty-fly-cummerbund.cyclic.app/api/v1";
+export const BASE_URL = "https://nutty-fly-cummerbund.cyclic.app/api/v1";
